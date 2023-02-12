8572a29ac3c75cccf5ef29bf1e4df7f142b683ad
chrisskiberg <58055220+chrisskiberg@users.noreply.github.com>
2023-02-12 09:24:57 +0100

    functional binary class function in the precision_recall function. TODO: Finding TP,TN,FP and FN and calculating precision and recall.

--- problem2.py
+++ problem2.py
@@ -23,10 +23,10 @@ from sklearn.preprocessing import label_binarize
 # DONE: Implement validation loss and accuracy in the training
 # DONE: Implement PR curves
 # ! TODO: Endre tilbake størrelsen på testset når ferdig å programmere funksionalitetene
+# DONE: Micro average over all classes and plot the average PR curve
 # ? DONE: Implement Area under the PR curve
 # ? Resultatene så litt rare ut, men ingenting er endret utenom test settet - Ja, ser ut som om det bare kommer an på test settet
 # TODO: Implement precision and recall (training-, test- and validation set?)
-
 
 # TODO: Implement saving function of the results (training and validation accuracy and loss, confusion matrix, PR curves)
 # TODO continuation: And the code to a file (epochs, training/validation split, neural network structure, loss function, optimizer)
@@ -88,29 +88,54 @@ def accuracy(network, dataloader):
 
         print(total_correct/total_instances)
 
-def precision_recall(y_pred, y_true): # ! jeg tror jeg må gjøre dette for hver klasse, men bør kanskje spørre om det er mulig med totalt?
-# ! er dette allerede implementert lenger ned? aner ikke hvordan jeg skal få total ut fra den
 
-# Trenger å lage arrays og for loops
-    total_truePositives=0
-    total_trueNegatives=0
-    total_falsePositives=0
-    total_falseNegatives=0
+# ! jeg tror jeg må gjøre dette for hver klasse, men bør kanskje spørre om det er mulig med totalt?
+def precision_recall(y_pred, y_true):
+    # ! er dette allerede implementert lenger ned? aner ikke hvordan jeg skal få total ut fra den
 
-    for i in range(len(y_pred)): # Se figur for å dobbeltsjekke
-        if y_pred[i]==y_true[i] and y_true[i]=="class":
-            total_truePositives+=1
-        elif y_pred[i]==y_true[i] and y_true[i]!="class":
-            total_trueNegatives+=1
-        elif y_pred[i]!=y_true[i] and y_pred[i]=="class":
-            total_falsePositives+=1
-        elif y_pred[i]!=y_true[i] and y_pred[i]!="class":
-            total_falseNegatives+=1
-    
-    total_precision=total_truePositives/(total_truePositives+total_falsePositives)
-    total_recall=total_truePositives/(total_truePositives+total_falseNegatives)
-    
+    # Trenger å lage arrays og for loops
 
+
+    precision_recall_arr=[[],[],[],[],[],[],[],[],[],[]] # 0,1,2,3,4,5,6,7,8 og 9, hvor indexsene er i hver liste 
+
+    # for x in range(len(precision_recall_arr)):
+    for x in range(1):
+        
+        # må lage liste hvor en klasse er 1 og de andre er 0
+        y_pred_class=[]
+        y_true_class=[]
+        for y in range(len(y_pred)):
+            if y_pred[y]==x:
+                y_pred_class.append(1)
+            else:
+                y_pred_class.append(0)
+
+            if y_true[y]==x:
+                y_true_class.append(1)
+            else:
+                y_true_class.append(0)
+
+        print("hei")
+
+        # truePositives = 0
+        # trueNegatives = 0
+        # falsePositives = 0
+        # falseNegatives = 0
+
+        # for i in range(len(y_pred)):  # Se figur for å dobbeltsjekke
+        #     if y_pred[i] == y_true[i] and y_true[i] == "class":
+        #         total_truePositives += 1
+        #     elif y_pred[i] == y_true[i] and y_true[i] != "class":
+        #         total_trueNegatives += 1
+        #     elif y_pred[i] != y_true[i] and y_pred[i] == "class":
+        #         total_falsePositives += 1
+        #     elif y_pred[i] != y_true[i] and y_pred[i] != "class":
+        #         total_falseNegatives += 1
+
+        # total_precision = total_truePositives / \
+        #     (total_truePositives+total_falsePositives)
+        # total_recall = total_truePositives / \
+        #     (total_truePositives+total_falseNegatives)
 
 
 transform = transforms.Compose([transforms.ToTensor(),
@@ -231,20 +256,28 @@ print('Accuracy of the network on the 10000 test images: %d %%'
       % (100 * correct / total))
 
 
+# ! uncomment under
 # jeg mener labels er ground truth
+# cf_matrix = confusion_matrix(y_true, y_pred)
+# df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1), index=[i for i in classes],
+#                      columns=[i for i in classes])
+# plt.figure(figsize=(12, 7))
+# sns.heatmap(df_cm, annot=True)
+# # plt.savefig('output.png')
+# plt.show()
 
-cf_matrix = confusion_matrix(y_true, y_pred)
-df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1), index=[i for i in classes],
-                     columns=[i for i in classes])
-plt.figure(figsize=(12, 7))
-sns.heatmap(df_cm, annot=True)
-# plt.savefig('output.png')
-plt.show()
+
 
 y_prob_samples = []
 # Det er 10 probabilities i et sample
 # Det er 10 samples i 1 batch
 # Det er 100 samples til sammen
+
+y_true = np.array([int(x) for x in y_true])
+# one hot encode the test data true labels
+y_true_binary = label_binarize(y_true, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+precision_recall(y_pred, y_true)
 
 for a in range(len(y_prob)):
     for b in range(len(y_prob[0])):
@@ -254,10 +287,6 @@ y_prob_samples = np.array(y_prob_samples)
 # Batch er 10, må dele opp slik at per sample og ikke per batch [[[...]]] ---> [[...]]
 # Det er til sammen 1000 samples, og dette er også i y_prob selv om ser litt vanskelig ut
 
-
-y_true = np.array([int(x) for x in y_true])
-# one hot encode the test data true labels
-y_true_binary = label_binarize(y_true, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 # Får riktig verdier, altså en verdi fra hver
 # print(y_true_binary[:, 0])
@@ -269,8 +298,10 @@ recall = dict()
 auc_precision_recall = []
 average_precision = dict()
 for i in range(len(classes)):
-    precision[i], recall[i], _ = precision_recall_curve(y_true_binary[:, i], y_prob_samples[:, i])
-    average_precision[i] = average_precision_score(y_true_binary[:, i], y_prob_samples[:, i])
+    precision[i], recall[i], _ = precision_recall_curve(
+        y_true_binary[:, i], y_prob_samples[:, i])
+    average_precision[i] = average_precision_score(
+        y_true_binary[:, i], y_prob_samples[:, i])
     # test_precision[i], test_recall[i], _ = precision_recall_curve(y_test_binary[:, i], y_test_score[:, i])
     plt.plot(recall[i], precision[i], lw=2, label='class {}'.format(i))
 
@@ -287,7 +318,8 @@ plt.show()
 precision["micro"], recall["micro"], _ = precision_recall_curve(
     y_true_binary.ravel(), y_prob_samples.ravel()
 )
-average_precision["micro"] = average_precision_score(y_true_binary, y_prob_samples, average="micro")
+average_precision["micro"] = average_precision_score(
+    y_true_binary, y_prob_samples, average="micro")
 
 display = PrecisionRecallDisplay(
     recall=recall["micro"],
